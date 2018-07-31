54cf38eec6006255f143cad3ce326087c3d9f27f
bastia_k <bastia_k@etna-alternance.net>
2018-07-31 21:32:44 +0200

    revoir andswerquestions

--- Be wise/Assets/Script/AnsweringQuestions.cs
+++ Be wise/Assets/Script/AnsweringQuestions.cs
@@ -5,92 +5,108 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
-public class AnsweringQuestions : MonoBehaviour {
+public class AnsweringQuestions : MonoBehaviour
+{
 
-	public string[] Questions = new string[]{"Be", "Have", "Other", "New", "Add"};
-	public string[] Answers = new string[]{"Etre", "Avoir", "Autre", "Nouveau", "Ajouter"};
+    public string[] Questions = new string[] { "Be", "Have", "Other", "New", "Add" };
+    public string[] Answers = new string[] { "Etre", "Avoir", "Autre", "Nouveau", "Ajouter" };
+    static public List<string> questionList = new List<string>();
 
+    public int maxAnswer = 0;
+    public bool NotFind = true;
 
-	public int maxAnswer = 0;
-	public bool NotFind = true;
+    public GameObject AnswerPanel;
+    public Text QuestionText, ErrorsText;
 
-	public GameObject AnswerPanel;
-	public Text QuestionText, ErrorsText;
+    static public int QuestionCounter = 0;
+    static public int GoodAnswerCounter = 0;
+    static public int falseAnswer = 0;
 
-	static public int QuestionCounter = 0;
-	static public int GoodAnswerCounter = 0;
-	static public int falseAnswer = 0;
+    public InputField inputed;
 
-	public InputField inputed;
+    // Use this for initialization
+    void Start()
+    {
+        QuestionText.text = Questions[QuestionCounter];
+        AnswerPanel.SetActive(false);
+        Debug.Log("question number : " + QuestionCounter);
+        Debug.Log("question bonne : " + GoodAnswerCounter);
+        Debug.Log("question fausse : " + falseAnswer);
+    }
 
-	// Use this for initialization
-	void Start () {
-		AnswerPanel.SetActive (false);
-		Debug.Log ("question number : " + QuestionCounter);
-		Debug.Log ("question bonne : " + GoodAnswerCounter);
-		Debug.Log ("question fausse : " + falseAnswer);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (AnswerPanel.activeSelf == true)
+        {
+            if (maxAnswer > 0 && falseAnswer < maxAnswer && NotFind && QuestionCounter < Questions.Length && QuestionCounter < Answers.Length)
+            {
+                if (inputed.isFocused && inputed.text != "" && Input.GetKey(KeyCode.Return))
+                {
+                    IsGoodAnswer();
+                }
+            }
+            else
+            {
+                UnPause();
+            }
+        }
+    }
 
-	// Update is called once per frame
-	void Update () {
-		if (AnswerPanel.activeSelf == true) {
-			if (maxAnswer > 0 && falseAnswer < maxAnswer && NotFind && QuestionCounter < Questions.Length && QuestionCounter < Answers.Length) {
-				if (inputed.isFocused && inputed.text != "" && Input.GetKey (KeyCode.Return)) {
-					IsGoodAnswer();
-				}
-			} else {
-				UnPause ();
-			}
-		}
-	}
+    void FixedUpdate()
+    {
+    }
 
-	void FixedUpdate() {
-	}
+    public void PauseForAnswer()
+    {
+        AnswerPanel.SetActive(true);
+        PrintErrors();
+        Time.timeScale = 0;
+        NotFind = true;
+    }
 
-	public void PauseForAnswer() {
-		AnswerPanel.SetActive (true);
-		PrintErrors ();
-		Time.timeScale = 0;
-		NotFind = true;
-		QuestionText.text = Questions [QuestionCounter];
-	}
+    public void UnPause()
+    {
+        questionList.Add(Questions[QuestionCounter]);
+        falseAnswer = 0;
+        QuestionCounter++;
+        AnswerPanel.SetActive(false);
+        Time.timeScale = 1;
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
-	public void UnPause() {
-		falseAnswer = 0;
-		AnswerPanel.SetActive (false);
-		Time.timeScale = 1;
-	}
+    private void PrintErrors()
+    {
+        string error = "";
+        if (maxAnswer > 0 && falseAnswer < maxAnswer)
+        {
+            int i = 0;
+            while (i < maxAnswer)
+            {
+                while (i < falseAnswer)
+                {
+                    error += "X ";
+                    i++;
+                }
+                error += (i + 1 < maxAnswer) ? "_ " : "_";
+                i++;
+            }
+        }
+        ErrorsText.text = error;
+    }
 
-	private void PrintErrors() {
-		string error = "";
-		if (maxAnswer > 0 && falseAnswer < maxAnswer) {
-			int i = 0;
-			while (i < maxAnswer) {
-				while (i < falseAnswer) {
-					error += "X ";
-					i++;
-				}
-				error += (i + 1 < maxAnswer) ? "_ " : "_";
-				i++;
-			}
-		}
-		ErrorsText.text = error;
-	}
-
-	private void IsGoodAnswer() {
-		if (inputed.text == Answers[QuestionCounter]) {
-			GoodAnswerCounter++;
-			QuestionCounter++;
-			NotFind = false;
-			Time.timeScale = 1 ;
-			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-		} else {
-			falseAnswer++;
-			if (maxAnswer > 0 && falseAnswer >= maxAnswer) {
-				QuestionCounter++;
-			}
-		}
-		PrintErrors ();
-		inputed.text = "";
-	}
+    private void IsGoodAnswer()
+    {
+        if (inputed.text == Answers[QuestionCounter])
+        {
+            GoodAnswerCounter++;
+            NotFind = false;
+        }
+        else
+        {
+            falseAnswer++;
+        }
+        PrintErrors();
+        inputed.text = "";
+    }
 }
--- Be wise/Assets/Script/Speaking.cs
+++ Be wise/Assets/Script/Speaking.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Speaking : MonoBehaviour {
+public class Speaking : MonoBehaviour
+{
 
-	public AnsweringQuestions question;
+    public AnsweringQuestions question;
 
-	void OnTriggerEnter (Collider collider) {
-	}
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(AnsweringQuestions.questionList.Contains(question.QuestionText.text));
+        print("--------------------");
+        foreach (string test in AnsweringQuestions.questionList)
+        {
+            Debug.Log(test);
+        }
+        print("--------------------");
+    }
 
-	void OnTriggerStay (Collider collider) {
-		if (collider.tag == "panel") {
-			if (Input.GetKey (KeyCode.Space) && AnsweringQuestions.QuestionCounter < question.maxAnswer && AnsweringQuestions.QuestionCounter < question.Questions.Length && AnsweringQuestions.QuestionCounter < question.Answers.Length) {
-				//collider.GetComponentInChildren<PanelText> ().PrintText ();
-				question.PauseForAnswer ();
-				Debug.Log ("Inside");
-			}
-		}
-	}
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "panel")
+        {
+            if (AnsweringQuestions.questionList.Contains(question.QuestionText.text) == false)
+            {
+                if (Input.GetKey(KeyCode.Space) && AnsweringQuestions.QuestionCounter < question.maxAnswer && AnsweringQuestions.QuestionCounter < question.Questions.Length && AnsweringQuestions.QuestionCounter < question.Answers.Length)
+                {
+                    //collider.GetComponentInChildren<PanelText>().PrintText();
+                    question.PauseForAnswer();
+                }
+            }
+            else if (AnsweringQuestions.questionList.Contains(question.QuestionText.text) == true)
+            {
+                Debug.Log("pas possible");
+            }
+        }
+    }
 }
