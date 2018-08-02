c87f8c52817cd282353bc75c7d7e9e6a352dbb87
bastia_k <bastia_k@etna-alternance.net>
2018-08-02 14:23:37 +0200

    musique continue a jouer au rechargement de la page

---
+++ Be wise/Assets/Script/AudioScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+        static AudioScript instance = null;
+
+        void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+}
